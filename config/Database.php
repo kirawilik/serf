eec610a88474b20c1c7b0295b76c5833a41cccc5
kirawilik <khadijakchini@gmail.com>
2026-04-14 23:02:54 +0100

    Connexion avec base de donner

--- config/Database.php
+++ config/Database.php
@@ -1,33 +1,25 @@
 <?php
-
 class Database {
-    private static ?Database $instance = null;
-    private PDO $pdo;
+    private static $instance = null;
+    private $pdo;
 
     private function __construct() {
-        try {
-            $this->pdo = new PDO(
-                "mysql:host=localhost;dbname=surf_taghazot;charset=utf8",
-                "root",
-                ""
-            );
-
-            $this->pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-            $this->pdo->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE, PDO::FETCH_ASSOC);
-
-        } catch (PDOException $e) {
-            die("Erreur DB : " . $e->getMessage());
-        }
+        $this->pdo = new PDO(
+            "mysql:host=localhost;dbname=surf_taghazot;charset=utf8",
+            "root",
+            ""
+        );
+        $this->pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
     }
 
-    public static function getInstance(): Database {
-        if (self::$instance === null) {
+    public static function getInstance() {
+        if (!self::$instance) {
             self::$instance = new Database();
         }
         return self::$instance;
     }
 
-    public function getConnection(): PDO {
+    public function getConnection() {
         return $this->pdo;
     }
 }
